9af992e9466f58a1d2a66277b9fac72f5598808b
netae <ehiheboloaugustar@gmail.com>
2020-04-01 22:01:49 +0100

    Node.js task 2

---
+++ mongo_db_create.js
@@ -0,0 +1,7 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/neta_eng";
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created by neta-eng");
+  db.close();
+});
