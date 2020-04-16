f06e5d9dee3e8bc3066b55bc09f8197adb55086e
netae <ehiheboloaugustar@gmail.com>
2020-04-16 08:51:53 +0100

    updated

--- updateInterns.js
+++ updateInterns.js
@@ -3,10 +3,10 @@ var url = "mongodb://127.0.0.1:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var coll = db.db("neta-eng");
+  var updatemovie = db.db("neta-eng");
   var myquery = { movie: "The Banker", year: "2020", rating: 8 };
   var newvalues = { $set: {movie: "War Room", year: "2019", rating: 9 } };
-  coll.collection("myMovies").updateOne(myquery, newvalues, function(err, res) {
+  updatemovie.collection("myMovies").updateOne(myquery, newvalues, function(err, res) {
     if (err) throw err;
     console.log("1 document updated");
     db.close();
