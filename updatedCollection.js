6d31e261261a32db83011ebb1959b636e85c8241
netae <ehiheboloaugustar@gmail.com>
2020-04-16 08:45:04 +0100

    Name updated

--- updatedCollection.js
+++ updatedCollection.js
@@ -3,8 +3,8 @@ var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var movies = db.db("neta-eng");
-  movies.collection("myMovies").find({}).toArray(function(err, result) {
+  var updatedMovie = db.db("neta-eng");
+  updatedMovie.collection("myMovies").find({}).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
